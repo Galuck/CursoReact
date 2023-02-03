8013296f38a1d703e70f24cad51cd7511cb9132c
Gabriel May Prochnow <GabrielMayPro@gmail.com>
2023-02-03 13:31:50 -0300

    atualização

--- cursoudemy/src/App.js
+++ cursoudemy/src/App.js
@@ -29,7 +29,7 @@ class App extends Component{
     this.state = {
         email: '',
         senha: '',
-        sexo: 'masculino'
+        sexo: ''
     }
 
     this.trocaEmail = this.trocaEmail.bind(this);
@@ -43,12 +43,32 @@ class App extends Component{
   render(){
     return(
       <div>
+
+        <h1>Novo usuario</h1>
+        <form>
+          <input type="text" value={this.state.nome} 
+              onChange></input>
+        </form>
+
         <h2>Login</h2>
         Email:
         <input type="email" name="email" value={this.state.email}></input>
         <br/>
         Senha:
-        <input type="password" name="senha" value={this.state.email}></input>
+        <input type="password" name="senha" value={this.state.senha}></input>
+        Sexo:
+        <select name="sexo" value={this.state.sexo}>
+          <option value="masculino">Masculino</option>
+          <option value="feminino">Feminino</option>
+          <option value="outro">Outro</option>
+        </select>
+
+        <div>
+          <h3>{this.state.email}</h3>
+          <h3>{this.state.senha}</h3>
+          <h3>{this.state.sexo}</h3>
+        </div>
+
       </div>
     );
   }
